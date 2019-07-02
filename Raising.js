19fad44ebc0a2d6410e4f38e76c10a2a757ee5b6
RaisingRay <newraisingray@gmail.com>
2019-07-02 21:54:01 +0200

    On branch master

--- Raising.js
+++ Raising.js
@@ -1,16 +1,51 @@
 //using my function
+
+
+
+function search(inputtext){
+var table=document.getElementsByTagName("table")[0];
+  searchTable(table,inputtext.value,0);
+}
+
+function searchTable(table,text,index){
+//  console.log(text);
+var rows=table.tBodies[0].getElementsByTagName("tr");
+for(var i=0;i<rows.length;i++){
+datas=rows[i].cells;
+var tdValue=datas[index].innerHTML;
+if(tdValue.startsWith(text)){
+  rows[i].style.display="";
+}else
+  rows[i].style.display="none";
+
+}
+
+//for(var i=0;i<rows.length;i++){
+//  var td= rows[i].getElementsByTagName("tr")[index];
+//  console.log(td.cells[0]);
+//    for(var j=0;j<td.length;j++){
+//      //if(text!=td[j])
+//      //  console.log('text= '+text+' td= '+td[j]);
+//      //else
+//      //  console.log('works')
+//    }
+//}
+
+
+}
+
+
+
 function addRowData(){
   var table=document.getElementsByTagName("table")[0];
   jsonList=
     [
           {"name":"fredj","last":"mahmoud"},
           {"name":"test2","last":"t"},
-          {"name":"lqs","last":"sd"}
+          {"name":"lqs","last":"sd"},
+          {"name":"fredjs","last":"sd"}
     ];
-
     addTable(table,jsonList);
-
-
 }
 
 
